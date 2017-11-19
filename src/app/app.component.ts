e37b53a2aec7f54a8960dfdb4595f857ef00ecf0
James Eschner <jameseschner@gmail.com>
2017-11-19 15:59:34 -0500

    add map to webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  lat: number = 43.0616993;
+  lng: number = -73.8069372;
 }
