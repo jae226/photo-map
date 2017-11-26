d4dcfc0d71d0cd272d7a88d0c04f9d3509b20e5c
James Eschner <jameseschner@gmail.com>
2017-11-26 15:34:39 -0500

    add photo carousel when market clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+export enum KEY_CODE {
+  ESCAPE = 27,
+  LEFT_ARROW = 37,
+  RIGHT_ARROW = 39
+}
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,28 @@ export class AppComponent {
   title: string = 'My first AGM project';
   lat: number = 43.0616993;
   lng: number = -73.8069372;
+  markerClicked: boolean = false;
+
+  ngOnInit() {
+    // Do any initialization
+  }
+
+  showPhotoViewer(): void {
+    this.markerClicked = true;
+  }
+
+  hidePhotoViewer(): void {
+    this.markerClicked = false;
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.keyCode === KEY_CODE.ESCAPE) {
+      this.markerClicked = false;
+    } else if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
+      // TODO move carousel ahead by one
+    } else if (event.keyCode === KEY_CODE.LEFT_ARROW) {
+      // TODO move carousel back by one
+    }
+  }
 }
